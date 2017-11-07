8fe826b3cbcacdea6bb48f65ae6f4a81f90f6b14
stevecook15 <cooksa@comcast.net>
2017-11-06 21:19:57 -0800

    Week 9 scores

--- 2017/scores.js
+++ 2017/scores.js
@@ -1,19 +1,19 @@
 
 //                  Record, Division Record
 
-var cookiemonRec   = [ 6, 2,  4, 2 ];   // Noah
-var predatorsRec   = [ 6, 2,  4, 2 ];   // Joe
-var belicheckRec   = [ 4, 4,  3, 3 ];   // Harrison
-var fatbastardsRec = [ 2, 6,  1, 5 ];   // Steve
+var predatorsRec   = [ 7, 2,  4, 2 ];   // Joe
+var cookiemonRec   = [ 6, 3,  4, 2 ];   // Noah
+var belicheckRec   = [ 4, 5,  3, 3 ];   // Harrison
+var fatbastardsRec = [ 3, 6,  1, 5 ];   // Steve
 
-var tenanusRec     = [ 6, 2,  5, 1 ];   // Jake
-var krustyRec      = [ 4, 4,  3, 3 ];   // Hunter - Screwed By Rodgers
-var iceotopesRec   = [ 3, 5,  3, 3 ];   // John
-var rolltideRec    = [ 1, 7,  1, 5 ];   // Daniel
+var tenanusRec     = [ 6, 3,  5, 1 ];   // Jake
+var krustyRec      = [ 5, 4,  3, 3 ];   // Hunter - Screwed By Rodgers
+var iceotopesRec   = [ 4, 5,  3, 3 ];   // John
+var rolltideRec    = [ 1, 8,  1, 5 ];   // Daniel
 
 var pfl_records=[
-      cookiemonRec,
       predatorsRec,
+      cookiemonRec,
       belicheckRec,
       fatbastardsRec,
 
@@ -24,8 +24,8 @@ var pfl_records=[
       ];
 
 var pfl_labels = [
-      "Cookie Monsters",
       "Predators",
+      "Cookie Monsters",
       "Discount Belicheck",
       "Fat Bastards",
 
@@ -39,35 +39,35 @@ var pfl_labels = [
 // Scoring
 
 //Div 1
-var fatbastards    = [ 124.05, 157.85, 114.80, 164.65, 142.65, 154.70, 139.65, 130.85 ];  // Steve
-var oppfatbastards = [ 131.45, 168.85, 187.80, 157.05, 151.60, 135.90, 154.60, 188.20 ];  // Steve
+var fatbastards    = [ 124.05, 157.85, 114.80, 164.65, 142.65, 154.70, 139.65, 130.85, 119.10 ];  // Steve
+var oppfatbastards = [ 131.45, 168.85, 187.80, 157.05, 151.60, 135.90, 154.60, 188.20, 117.60 ];  // Steve
 
-var belicheck    = [ 131.45, 127.05, 166.35, 131.80, 139.60, 135.90, 126.80, 144.10 ];  // Harrsion
-var oppbelicheck = [ 124.05, 110.50, 142.70, 105.85, 157.45, 154.70, 161.90, 174.85 ];  // Harrsion
+var belicheck    = [ 131.45, 127.05, 166.35, 131.80, 139.60, 135.90, 126.80, 144.10, 87.10 ];  // Harrsion
+var oppbelicheck = [ 124.05, 110.50, 142.70, 105.85, 157.45, 154.70, 161.90, 174.85, 144.60 ];  // Harrsion
 
-var predators    = [ 150.80, 168.80, 142.70, 154.55, 123.25, 130.65, 154.60, 174.85 ];  // Joe
-var opppredators = [ 141.00, 157.85, 166.35, 129.60, 115.90, 149.20, 139.65, 144.10 ];  // Joe
+var predators    = [ 150.80, 168.80, 142.70, 154.55, 123.25, 130.65, 154.60, 174.85, 164.80 ];  // Joe
+var opppredators = [ 141.00, 157.85, 166.35, 129.60, 115.90, 149.20, 139.65, 144.10, 149.70 ];  // Joe
 
-var cookiemon    = [ 141.00, 110.50, 187.80, 162.80, 178.85, 149.20, 161.90, 188.20 ];  // Noah
-var oppcookiemon = [ 150.80, 127.05, 114.80, 135.65, 129.55, 130.65, 126.80, 130.85 ];  // Noah
+var cookiemon    = [ 141.00, 110.50, 187.80, 162.80, 178.85, 149.20, 161.90, 188.20, 131.60 ];  // Noah
+var oppcookiemon = [ 150.80, 127.05, 114.80, 135.65, 129.55, 130.65, 126.80, 130.85, 160.20 ];  // Noah
 
 //Div 2
-var iceotopes    = [ 117.95, 113.15, 148.45, 105.85, 115.90, 122.30, 135.20, 123.95 ];  // John
-var oppiceotopes = [ 131.65, 123.65, 106.85, 131.80, 123.25, 201.85, 100.10, 115.80 ];  // John
+var iceotopes    = [ 117.95, 113.15, 148.45, 105.85, 115.90, 122.30, 135.20, 123.95, 160.20 ];  // John
+var oppiceotopes = [ 131.65, 123.65, 106.85, 131.80, 123.25, 201.85, 100.10, 115.80, 131.60 ];  // John
 
-var tenanus    = [ 131.65, 143.45, 148.45, 157.05, 157.45, 201.85, 181.30, 157.75 ];  // Jake
-var opptenanus = [ 117.95, 118.75, 151.40, 164.65, 139.60, 122.30, 130.95, 146.55 ];  // Jake
+var tenanus    = [ 131.65, 143.45, 148.45, 157.05, 157.45, 201.85, 181.30, 157.75, 149.70 ];  // Jake
+var opptenanus = [ 117.95, 118.75, 151.40, 164.65, 139.60, 122.30, 130.95, 146.55, 164.80 ];  // Jake
 
-var rolltide    = [ 111.40, 118.75, 106.85, 129.60, 129.55, 114.55, 130.95, 115.80 ];   // Daniel
-var opprolltide = [ 154.50, 143.45, 148.45, 154.55, 178.85, 112.15, 181.30, 123.95 ];   // Daniel
+var rolltide    = [ 111.40, 118.75, 106.85, 129.60, 129.55, 114.55, 130.95, 115.80, 117.60 ];   // Daniel
+var opprolltide = [ 154.50, 143.45, 148.45, 154.55, 178.85, 112.15, 181.30, 123.95, 119.10 ];   // Daniel
 
-var krusty    = [ 154.50, 123.65, 151.40, 135.65, 151.60, 112.15, 100.10, 146.55 ];   // Hunter Screwed
-var oppkrusty = [ 111.40, 113.15, 148.45, 162.80, 142.65, 114.55, 135.20, 157.75 ];   // Hunter
+var krusty    = [ 154.50, 123.65, 151.40, 135.65, 151.60, 112.15, 100.10, 146.55, 144.60 ];   // Hunter Screwed
+var oppkrusty = [ 111.40, 113.15, 148.45, 162.80, 142.65, 114.55, 135.20, 157.75, 87.10 ];   // Hunter
 
 
 var pfl_owners = [
-      "Noah",       // Cookie Monsters
       "Joe",        // Predators
+      "Noah",       // Cookie Monsters
       "Harrison",   // Belicheck
       "Steve",      // Fat Bastards
 
@@ -78,8 +78,8 @@ var pfl_owners = [
       ];
 
 var pfl_scores=[
-        cookiemon,
         predators,
+        cookiemon,
         belicheck,
         fatbastards,
 
@@ -90,8 +90,8 @@ var pfl_scores=[
      ];
 
 var opp_scores=[
-        oppcookiemon,
         opppredators,
+        oppcookiemon,
         oppbelicheck,
         oppfatbastards,
 
@@ -107,8 +107,8 @@ var opp_scores=[
 // 3 - clinched first round bye
 // 4 - eliminated from playoffs
 var playoffs=[
-              0,   // cookiemon
               0,   // predators
+              0,   // cookiemon
               0,   // belicheck
               0,   // bastards
 
@@ -134,7 +134,7 @@ var week7_txt = "Another week where the good teams won and the bad teams lost, n
 
 var week8_txt = "At this point there's virtually 3 spots locked up for the playoffs as all 3 have a 2 game lead and no sign that any of the rest of us can challenge them.  Two teams are basically out of it already as even if they won out they still probably couldn't catch the top 3, leaving just 1 spot between the remaining 5 teams! Good battle among the middle 3 for the last spot though as they're all within 1 win of that4th spot.";
 
-var week9_txt = "";
+var week9_txt = "Guess if you're Tenacious enough, the sphincter will hold up and lead you to the top.  On the other hand, if the Tides still out you get Flattened! And in this weeks news from the weird the topes take down the highest scoring team in the league! Not to mention that stellar discounted effort the Bellies put in!";
 
 var week10_txt = "";
 
