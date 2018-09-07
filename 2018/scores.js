372a91b1e65a904de30693e20c13b676d6a72b16
stevecook15 <cooksa@comcast.net>
2018-09-06 22:57:07 -0400

    Updated schedule

--- 2018/scores.js
+++ 2018/scores.js
@@ -194,47 +194,47 @@ var sten = tenanus;
 var snub = thenubs;
 var sing = inglorious;
 
-var week01Teams = new Array(dfat, dbel, dice, dten, dcom, dpre, drol, dkru, ding, dnub);
-var week01Scores = new Array(sfat[0], sbel[0], sice[0], sten[0], scom[0], spre[0], srol[0], skru[0], snub[0], sing[0]);
+var week01Teams = new Array(dfat, dkru, dice, dbel, dcom, dpre, drol, ding, dten, dnub);
+var week01Scores = new Array(sfat[0], skru[0], sice[0], sbel[0], scom[0], spre[0], srol[0], sing[0], sten[0], snub[0]);
 
-var week02Teams = new Array(dfat, dpre, dice, dkru, dcom, dbel, drol, dten, ding, dnub); 
-var week02Scores = new Array(sfat[1], spre[1], sice[1], skru[1], scom[1], sbel[1], srol[1], sten[1], snub[1], sing[1]);
+var week02Teams = new Array(dfat, dpre, dice, dnub, dcom, dbel, drol, dkru, dten, ding); 
+var week02Scores = new Array(sfat[1], spre[1], sice[1], snub[1], scom[1], sbel[1], srol[1], skru[1], sten[1], sing[1]);
 
-var week03Teams = new Array(dfat, dcom, dice, drol, dpre, dbel, dkru, dten, ding, dnub);
-var week03Scores = new Array(sfat[2], scom[2], sice[2], srol[2], spre[2], sbel[2], skru[2], sten[2], snub[2], sing[2]);
+var week03Teams = new Array(dfat, dcom, dice, drol, dpre, dten, dkru, dbel, dnub, ding);
+var week03Scores = new Array(sfat[2], scom[2], sice[2], srol[2], spre[2], sten[2], skru[2], sbel[2], snub[2], sing[2]);
 
-var week04Teams = new Array(fat, ten, ice, bel, com, kru, rol, pre, ding, dnub);
-var week04Scores = new Array(sfat[3], sten[3], sice[3], sbel[3], scom[3], skru[3], srol[3], spre[3], snub[3], sing[3]);
+var week04Teams = new Array(fat, ice, com, ing, nub, pre, kru, ten, bel, rol);
+var week04Scores = new Array(sfat[3], sice[3], scom[3], sing[3], snub[3], spre[3], skru[3], sten[3], sbel[3], srol[3]);
 
-var week05Teams = new Array(fat, kru, ice, pre, com, rol, bel, ten, ding, dnub);
-var week05Scores = new Array(sfat[4], skru[4], sice[4], spre[4], scom[4], srol[4], sbel[4], sten[4], snub[4], sing[4]);
+var week05Teams = new Array(fat, rol, ice, com, ing, pre, kru, nub, bel, ten);
+var week05Scores = new Array(sfat[4], srol[4], sice[4], scom[4], sing[4], spre[4], skru[4], snub[4], sbel[4], sten[4]);
 
-var week06Teams = new Array(dfat, dbel, dice, dten, dcom, dpre, drol, dkru, ding, dnub);
-var week06Scores = new Array(sfat[5], sbel[5], sice[5], sten[5], scom[5], spre[5], srol[5], skru[5], snub[5], sing[5]);
+var week06Teams = new Array(dfat, dbel, dice, ding, com, nub, dpre, dkru, drol, dten);
+var week06Scores = new Array(sfat[5], sbel[5], sice[5], sing[5], scom[5], snub[5], spre[5], skru[5], srol[5], sten[5]);
 
-var week07Teams = new Array(dfat, dpre, dice, dkru, dcom, dbel, drol, dten, ding, dnub);
-var week07Scores = new Array(sfat[6], spre[6], sice[6], skru[6], scom[6], sbel[6], srol[6], sten[6], snub[6], sing[6]);
+var week07Teams = new Array(fat, ing, dice, dten, dcom, dkru, dpre, dbel, drol, dten);
+var week07Scores = new Array(sfat[6], sing[6], sice[6], sten[6], scom[6], skru[6], spre[6], sbel[6], srol[6], sten[6]);
 
-var week08Teams = new Array(dfat, dcom, dice, drol, dpre, dbel, dkru, dten, ding, dnub);
-var week08Scores = new Array(sfat[7], scom[7], sice[7], srol[7], spre[7], sbel[7], skru[7], sten[7], snub[7], sing[7]);
+var week08Teams = new Array(dfat, dkru, dice, ding, dcom, dpre, bel, nub, drol, dten);
+var week08Scores = new Array(sfat[7], skru[7], sice[7], sing[7], scom[7], spre[7], sbel[7], snub[7], srol[7], sten[7]);
 
-var week09Teams = new Array(fat, rol, ice, com, pre, ten, kru, bel, ding, dnub);
-var week09Scores = new Array(sfat[8], srol[8], sice[8], scom[8], spre[8], sten[8], skru[8], sbel[8], snub[8], sing[8]);
+var week09Teams = new Array(fat, nub, ice, kru, com, ten, rol, pre, bel, ing);
+var week09Scores = new Array(sfat[8], snub[8], sice[8], skru[8], scom[8], sten[8], srol[8], spre[8], sbel[8], sing[8]);
 
-var week10Teams = new Array(fat, ice, com, ten, rol, bel, pre, kru, ding, dnub);
-var week10Scores = new Array(sfat[9], sice[9], scom[9], sten[9], srol[9], sbel[9], spre[9], skru[9], snub[9], sing[9]);
+var week10Teams = new Array(fat, ten, ice, pre, com, rol, kru, ing, bel, nub);
+var week10Scores = new Array(sfat[9], sten[9], sicm[9], spre[9], scom[9], srol[9], skru[9], sing[9], sbel[9], snub[9]);
 
-var week11Teams = new Array(dfat, dbel, dice, dten, dcom, dpre, drol, dkru, ding, dnub);
-var week11Scores = new Array(sfat[10], sbel[10], sice[10], sten[10], scom[10], spre[10], srol[10], skru[10], snub[10], sing[10]);
+var week11Teams = new Array(dfat, dpre, dice, dnub, dcom, dbel, kru, ten, drol, ding);
+var week11Scores = new Array(sfat[10], spre[10], sice[10], snub[10], scom[10], sbel[10], skru[10], sten[10], srol[10], sing[10]);
 
-var week12Teams = new Array(dfat, dpre, dice, dkru, dcom, dbel, drol, dten, ding, dnub);
-var week12Scores = new Array(sfat[11], spre[11], sice[11], skru[11], scom[11], sbel[11], srol[11], sten[11], snub[11], sing[11]);
+var week12Teams = new Array(dfat, dcom, dice, dten, rol, pre, dkru, dbel, ding, dnub);
+var week12Scores = new Array(sfat[11], scom[11], sice[11], sten[11], srol[11], spre[11], skru[11], sbel[11], sing[11], snub[11]);
 
-var week13Teams = new Array(dfat, dcom, dice, drol, dpre, dbel, dkru, dten, ding, dnub);
-var week13Scores = new Array(sfat[12], scom[12], sice[12], srol[12], spre[12], sbel[12], skru[12], sten[12], snub[12], sing[12]);
+var week13Teams = new Array(dfat, dbel, ice, com, dkru, dpre, drol, dnub, dten, ding);
+var week13Scores = new Array(sfat[12], sbel[12], sice[12], scom[12], skru[12], spre[12], srol[12], snub[12], sten[12], sing[12]);
 
-var week14Teams = new Array(fat, ten, ice, bel, com, kru, rol, pre, ding, dnub);
-var week14Scores = new Array(sfat[13], sten[13], sice[13], sbel[13], scom[13], skru[13], srol[13], spre[13], snub[13], sing[13]);
+var week14Teams = new Array(fat, ing, dice, drol, dcom, dkru, dpre, dbel, dten, dnub);
+var week14Scores = new Array(sfat[13], sing[13], sice[13], srol[13], scom[13], skru[13], spre[13], sbel[13], sten[13], snub[13]);
 
 var weekly_dates = [ "Thursday, September 6th",  "Thursday, September 13th", "Thursday, September 20th",
                      "Thursday, September 27th", "Thursday, October 4th",    "Thursday, October 11th", 
