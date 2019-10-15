384c65509072b3ad17e0dc90fea39c33e7cdcf55
stevecook15 <cooksa@comcast.net>
2019-10-14 23:28:03 -0400

    Week 6 scores

--- 2019/new_scores.js
+++ 2019/new_scores.js
@@ -3,17 +3,17 @@
 // Create map with team and array each weeks opponent:  key fat, array ice, roll, etc...
 // Create map with team and each weeks score (name based on the same name as in the map (eg roll))
 
-var belScores = [ 117.00, 142.15, 147.70, 103.35, 170.25 ];
-var makScores = [ 181.80, 138.50, 169.40, 154.15, 153.40 ];
-var rolScores = [ 125.60,  97.10, 139.40, 91.30, 166.40 ];
-var kekScores = [ 102.55, 144.95, 114.05, 72.20, 75.95 ];
-var iceScores = [ 121.75, 126.80, 158.60, 132.35, 125.60 ];
+var belScores = [ 117.00, 142.15, 147.70, 103.35, 170.25, 82.05 ];
+var makScores = [ 181.80, 138.50, 169.40, 154.15, 153.40, 121.60 ];
+var rolScores = [ 125.60,  97.10, 139.40, 91.30, 166.40, 160.15 ];
+var kekScores = [ 102.55, 144.95, 114.05, 72.20, 75.95, 106.95 ];
+var iceScores = [ 121.75, 126.80, 158.60, 132.35, 125.60, 170.00 ];
 
-var nubScores = [ 194.30, 157.95, 143.75, 121.45, 133.20 ];
-var preScores = [ 109.65, 117.85, 138.35, 141.15, 89.70 ];
-var tenScores = [  85.90, 132.85, 118.10, 99.15, 153.15 ];
-var kruScores = [ 143.70, 142.30, 155.75, 144.65, 155.95 ];
-var fatScores = [ 146.50,  99.15, 108.05, 135.05, 188.45 ];
+var nubScores = [ 194.30, 157.95, 143.75, 121.45, 133.20, 106.55 ];
+var preScores = [ 109.65, 117.85, 138.35, 141.15, 89.70, 146.80 ];
+var tenScores = [  85.90, 132.85, 118.10, 99.15, 153.15, 148.25 ];
+var kruScores = [ 143.70, 142.30, 155.75, 144.65, 155.95, 130.80 ];
+var fatScores = [ 146.50,  99.15, 108.05, 135.05, 188.45, 81.70 ];
 
 
 // 0 - not in playoffs yet
@@ -68,7 +68,12 @@ var week5_txt = "Down go Frazier, down goes frazier (I know, a lost reference to
                 "Meanwhile, in the I'm not very good but I'm better than you bowl, the Ice preyed on the " +
                 "Preds, and the Tide rolled over the Tentative Anus.";
 
-var week6_txt = "";
+var week6_txt = "From first to worst, the Flat score 100 points less than last week, handing the Nubs a hard(ly) "+
+                "deserved victory (as the only score 104 points themselves). The other two 4-1 teams went down, " +
+                "leaving the Nubs in sole posession of first place.  The Krusty Bellys both got Rolled to sub-500 " +
+                "Predators. With the Mack getting Iced the standings closed up the battle for playoff spots. " +
+                "Speaking about playoffs, the Anus pooped on Keke in the Tank for Tua bowl (does that work for " +
+                "the PFL too?) with Keke doing their best Miami Bengals impression.";
 
 var week7_txt = "";
 
