cbb6230dc0fc4769bde594e50a16b6b1c018da8d
stevecook15 <cooksa@comcast.net>
2019-10-28 23:47:45 -0400

    Week 8 scores

--- 2019/new_scores.js
+++ 2019/new_scores.js
@@ -4,17 +4,17 @@
 // Create map with team and array each weeks opponent:  key fat, array ice, roll, etc...
 // Create map with team and each weeks score (name based on the same name as in the map (eg roll))
 
-var belScores = [ 117.00, 142.15, 147.70, 103.35, 170.25, 82.05, 115.00 ];
-var makScores = [ 181.80, 138.50, 169.40, 154.15, 153.40, 121.60, 97.15 ];
-var iceScores = [ 121.75, 126.80, 158.60, 132.35, 125.60, 170.00, 94.65 ];
-var rolScores = [ 125.60,  97.10, 139.40, 91.30, 166.40, 160.15, 85.75 ];
-var kekScores = [ 102.55, 144.95, 114.05, 72.20, 75.95, 106.95, 94.05 ];
+var belScores = [ 117.00, 142.15, 147.70, 103.35, 170.25, 82.05, 115.00, 137.90 ];
+var makScores = [ 181.80, 138.50, 169.40, 154.15, 153.40, 121.60, 97.15, 135.45 ];
+var iceScores = [ 121.75, 126.80, 158.60, 132.35, 125.60, 170.00, 94.65, 112.45 ];
+var rolScores = [ 125.60,  97.10, 139.40, 91.30, 166.40, 160.15, 85.75, 131.90 ];
+var kekScores = [ 102.55, 144.95, 114.05, 72.20, 75.95, 106.95, 94.05, 137.35 ];
 
-var nubScores = [ 194.30, 157.95, 143.75, 121.45, 133.20, 106.55, 93.60 ];
-var kruScores = [ 143.70, 142.30, 155.75, 144.65, 155.95, 130.80, 100.80 ];
-var preScores = [ 109.65, 117.85, 138.35, 141.15, 89.70, 146.80, 128.65 ];
-var fatScores = [ 146.50,  99.15, 108.05, 135.05, 188.45, 81.70, 139.90 ];
-var tenScores = [  85.90, 132.85, 118.10, 99.15, 153.15, 148.25, 143.75 ];
+var nubScores = [ 194.30, 157.95, 143.75, 121.45, 133.20, 106.55, 93.60, 119.85 ];
+var kruScores = [ 143.70, 142.30, 155.75, 144.65, 155.95, 130.80, 100.80, 141.65 ];
+var preScores = [ 109.65, 117.85, 138.35, 141.15, 89.70, 146.80, 128.65, 146.15 ];
+var fatScores = [ 146.50,  99.15, 108.05, 135.05, 188.45, 81.70, 139.90, 136.80 ];
+var tenScores = [  85.90, 132.85, 118.10, 99.15, 153.15, 148.25, 143.75, 139.35 ];
 
 
 // 0 - not in playoffs yet
@@ -24,16 +24,16 @@ var tenScores = [  85.90, 132.85, 118.10, 99.15, 153.15, 148.25, 143.75 ];
 // 4 - eliminated from playoffs
 var playoffs = [
         0,  //belicheck
-        0,  //retmack
-        0,  //iceotopes
         0,  //rolltide
+        0,  //iceotopes
+        0,  //retmack
         0,  //kekelove
 
         0,  //thenubs
-        0,  //krusty
         0,  //predators
         0,  //fatbastards
-        0   //tenanus
+        0,  //tenanus
+        0   //krusty
      ];
 
 
@@ -82,7 +82,13 @@ var week7_txt = "A pretty low scoring week - lowest high score of the week yet. 
                 "And with the Nubs (with an eyebrow raising of a 102 points them selves) and the Bellis going down it seems " +
                 "like the battle for the playoffs is tightening up...";
 
-var week8_txt = "";
+var week8_txt = "OK, which teams aren't 4-4?  Six of the ten are 4-4, two more are within a game, bookended by two " +
+                "outliers for first and last. So, 4-4 is currently a playoff team - better start running up your " +
+                "scores as total pts is the first tie-breaker! Keke would have loved another half a pt, which would " +
+                "have made it even closer, but they coug'ed it sadly. The Nubs were shaved to a stubble for the second " +
+                "straight week to drop out of first for the first time this year, while the Flats victory got them to " +
+                ".500 for the first time this year. And amazingly, the Mack despite scoring more points than anyone are " +
+                "now 2 games under .500! Say what?";
 
 var week9_txt = "";
 
