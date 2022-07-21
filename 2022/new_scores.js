f85e7da04d47f7cda58020f2715bc4e8143c208a
Steve Cook <cooksa@comcast.net>
2022-07-21 09:54:14 -0400

    First update for 2022 Season

---
+++ 2022/new_scores.js
@@ -0,0 +1,78 @@
+"use strict";
+
+//TODO: Add a more plots
+//TODO: Sort division record by score, div record and total pts
+//TODO: fix bar chart label below X axis
+
+// Create an array of teamNames  (see scores.js)
+// Create map with team and array each weeks opponent:  key fat, array ice, roll, etc...
+// Create map with team and each weeks score (name based on the same name as in the map (eg roll))
+
+var preScores = [ 0.0 ];  // Predators
+var belScores = [ 0.0 ];  // Discount Belicheck
+var tenScores = [ 0.0 ];  // Tenacious Anus
+var cocScores = [ 0.0 ];  // Coco Browns
+var seaScores = [ 0.0 ];  // Seamen (Adam)
+
+var iceScores = [ 0.0 ];  // Iceotopes
+var fatScores = [ 0.0 ];  // Phat Bastards
+var porScores = [ 0.0 ];  // Porkchops
+var rolScores = [ 0.0 ];  // Roll Tide
+var pepScores = [ 0.0 ];  // Jabronis Fuller 
+
+// 0 - not in playoffs yet
+// 1 - clinched playoff spot
+// 2 - clinched division
+// 3 - clinched first round bye
+// 4 - eliminated from playoffs
+//TODO: Apparently this isn't sorted along with the tems...
+var playoffs = [
+        0,  //alien seamen
+        0,  //tenanus
+        0,  //predators
+        0,  //cocobrown
+        0,  //belicheck
+        
+        9,  //porkchops
+        9,  //iceotops
+        9,  //fatbastards
+        9,  //rolltide
+        9   //fullpepsi
+     ];
+
+
+var week0_txt = "Welcome to the 2022 Phantom League.";
+
+var week1_txt = "Welcome once again to the exciting PFL league for the 2022 season.  While we know you're all looking forward " +
+                "to pulling down the venerable PFL Championship this year, sadly none of you will, as the Phat have a guarenteed " +
+                "lock on that (at least on this web site). Don't care what that damn Yahoo site indicates... Good luck to you all " +
+                "in securing second place!";
+
+var week2_txt = "";
+
+var week3_txt = "";
+
+var week4_txt = "";
+ 
+var week5_txt = "";
+
+var week6_txt = "";
+
+var week7_txt = "";
+
+var week8_txt = "";
+
+var week9_txt = "";
+
+var week10_txt = "";
+
+var week11_txt = "";
+
+var week12_txt = "";
+
+var week13_txt = "";
+
+var week14_txt = "";
+
+var week15_txt = "";
+
