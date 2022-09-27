ac7f063d9ef2000d4af339cdc6903bf86730949b
Steve Cook <cooksa@comcast.net>
2022-09-27 09:07:39 -0400

    Week 3 scores

--- 2022/new_scores.js
+++ 2022/new_scores.js
@@ -8,17 +8,17 @@
 // Create map with team and array each weeks opponent:  key fat, array ice, roll, etc...
 // Create map with team and each weeks score (name based on the same name as in the map (eg roll))
 
-var cocScores = [ 131.00, 123.65 ];  // Coco Browns (Andy)
-var fatScores = [ 132.40, 153.40 ];  // Fat Bastards (Steve)
-var preScores = [ 148.55, 108.55 ];  // Predators (Joe)
-var rolScores = [ 102.85, 189.50 ];  // Roll Tide (Daniel)
-var seaScores = [ 100.35, 119.35 ];  // Seamen (Adam)
+var cocScores = [ 131.00, 123.65, 122.50 ];  // Coco Browns (Andy)
+var fatScores = [ 132.40, 153.40, 132.85 ];  // Phat Bastards (Steve)
+var preScores = [ 148.55, 108.55,  94.70 ];  // Predators (Joe)
+var rolScores = [ 102.85, 189.50, 141.60 ];  // Roll Tide (Daniel)
+var seaScores = [ 100.35, 119.35, 100.30 ];  // Seamen (Adam)
 
-var iceScores = [ 113.75, 128.15 ];  // Iceotopes (John)
-var jabScores = [ 142.85, 157.45 ];  // Najee By Nature  (Matt)
-var belScores = [ 110.85, 126.30 ];  // Discount Belicheck (Harrision)
-var porScores = [ 134.05,  85.70 ];  // Porkchops (Noah)
-var tenScores = [ 142.90, 140.70 ];  // Tenacious Anus (Jacob)
+var iceScores = [ 113.75, 128.15,  92.50 ];  // Iceotopes (John)
+var jabScores = [ 142.85, 157.45, 142.10 ];  // Najee By Nature  (Matt)
+var belScores = [ 110.85, 126.30, 126.65 ];  // Discount Belicheck (Harrision)
+var porScores = [ 134.05,  85.70,  94.75 ];  // Porkchops (Noah)
+var tenScores = [ 142.90, 140.70,  90.55 ];  // Tenacious Anus (Jacob)
 
 
 // 0 - not in playoffs yet
@@ -28,7 +28,7 @@ var tenScores = [ 142.90, 140.70 ];  // Tenacious Anus (Jacob)
 // 4 - eliminated from playoffs
 
 var cocPlayoffs = 0;  // Coco Browns (Andy)
-var fatPlayoffs = 0;  // Fat Bastards (Steve)
+var fatPlayoffs = 0;  // Phat Bastards (Steve)
 var prePlayoffs = 0;  // Predators (Joe)
 var rolPlayoffs = 0;  // Roll Tide (Daniel)
 var seaPlayoffs = 0;  // Seamen (Adam)
@@ -45,15 +45,15 @@ var tenPlayoffs = 0;  // Tenacious Anus (Jacob)
 var week0_txt = "Welcome to the 2022 Phantom League.";
 
 var week1_txt = "Welcome once again to the exciting PFL league for the 2022 season.  While we know you're all looking forward " +
-                "to pulling down the venerable PFL Championship this year, sadly none of you will, as the Fat have a guaranteed " +
+                "to pulling down the venerable PFL Championship this year, sadly none of you will, as the Phat have a guaranteed " +
                 "lock on that (at least on <i><b>this</b></i> web site. Don't care what that damn Yahoo site says..., what do they know anyway?) " +
                 "Good luck to you all in securing second place! <br><br>Najee, Predators amd Anus all scored in the 140's and had comfortable wins, " +
-		          "but the other two week 1 winners (Fat and Ice) had probably ended up pulling out (what remains of) their hair by the end of " +
+		          "but the other two week 1 winners (Phat and Ice) had probably ended up pulling out (what remains of) their hair by the end of " +
                 "the Hawks/Broncos game (Go Hawks!) as the both pulled out close wins with the Bastards cussing the PPR rule the whole way... " +
                 "1 yard RB dumpoffs worth 1.1 pts??? Really?? Grumble, grumble, grumble...";
 
 var week2_txt = "So apparently the secret to high scoring in the PFL is to not check your lineup and leave two open spots... and just Roll with " +
-                "it scoring 189 points? Bastards! The Fat beat their projected score by 25 and still lose by 36? Or another way of looking " +
+                "it scoring 189 points? Bastards! The Phat beat their projected score by 25 and still lose by 36? Or another way of looking " +
                 "at it, two of the Tides players outscored the entire Chops lineup (and if your loser pool pick had the Pork being the " +
                 "first team to not break a 100 for the year, you're a winner, winner, chicken dinner). The Bellys weren't complaining " +
                 "though as their sad effort still chopped up a win against their bro!<br><br>Speaking of sad efforts, the Browns outdid " +
@@ -62,7 +62,15 @@ var week2_txt = "So apparently the secret to high scoring in the PFL is to not c
                 "the Seaboys, and the Anus beat the Topes who lost despite their top 3 players scoring 83 points (which just has to Hurts!).<br>";
 
 
-var week3_txt = "";
+var week3_txt = "Well, much lower scores this week as four teams fail to break 100 and a fifth with their toes touching that line...  But congrats " +
+                "to the Chops as they still ekeed out a <i>'victory'</i> with their 94 point effort icing out the Topes. On the other hand, the Tens league low  " +
+                "score for the week didn't work so well matched up against the Naturals league high score for the week. It just kinda works that " +
+                "way I guess...<br><br>The Tide, in their ongoing groundbreaking and innovative lineup strategies, replaced an out for the week wide receiver for, " +
+                "you guessed it, another injured out for the week wide receiver! Pure Genious as it led to yet another win, this time over the Lakemen, " +
+                "though there's now some feeling that their team should be renamed to <i>Roll Lamar</i>... Lamars bye week is gonna be a killer!<br><br>In the other " +
+                "two matchups, the Preds got ho-hum efforts out of most of their players and a virtual goose egg out of their #1 WR to easily lose to the Phat's " +
+                "mostly mundane effort, and the Clearance Coaches slipped by the Brownies (just not much to say about this one...).<br><br>So, after just three weeks " +
+                " we're down to one undefeated team, and one defeated team, the rest of us middling around the middle... long season still though!<br>";
 
 var week4_txt = "";
  
