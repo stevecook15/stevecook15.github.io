4749ad238178dd69d9c6f9c1e1f7ad778103b6ac
Steve Cook <cooksa@comcast.net>
2022-10-24 23:40:44 -0400

    Week 7 scores
    Hope this works

--- 2022/new_scores.js
+++ 2022/new_scores.js
@@ -8,17 +8,17 @@
 // Create map with team and array each weeks opponent:  key fat, array ice, roll, etc...
 // Create map with team and each weeks score (name based on the same name as in the map (eg roll))
 
-var cocScores = [ 131.00, 123.65, 122.50, 104.85, 134.30, 136.80 ];  // Coco Browns (Andy)
-var fatScores = [ 132.40, 153.40, 132.85, 166.25, 122.05, 142.10 ];  // Phat Bastards (Steve)
-var preScores = [ 148.55, 108.55,  94.70, 193.05, 118.50, 116.05 ];  // Predators (Joe)
-var rolScores = [ 102.85, 189.50, 141.60, 127.20, 123.40, 129.40 ];  // Roll Tide (Daniel)
-var seaScores = [ 100.35, 119.35, 100.30, 116.25, 126.00,  97.50 ];  // Seamen (Adam)
+var cocScores = [ 131.00, 123.65, 122.50, 104.85, 134.30, 136.80, 134.45 ];  // Coco Browns (Andy)
+var fatScores = [ 132.40, 153.40, 132.85, 166.25, 122.05, 142.10, 136.05 ];  // Phat Bastards (Steve)
+var preScores = [ 148.55, 108.55,  94.70, 193.05, 118.50, 116.05, 143.85 ];  // Predators (Joe)
+var rolScores = [ 102.85, 189.50, 141.60, 127.20, 123.40, 129.40,  86.10 ];  // Roll Tide (Daniel)
+var seaScores = [ 100.35, 119.35, 100.30, 116.25, 126.00,  97.50,  98.70 ];  // Seamen (Adam)
 
-var iceScores = [ 113.75, 128.15,  92.50, 123.70, 136.35,  92.85 ];  // Iceotopes (John)
-var jabScores = [ 142.85, 157.45, 142.10, 120.15, 181.30, 113.55 ];  // Najee By Nature  (Matt)
-var belScores = [ 110.85, 126.30, 126.65, 126.10, 125.00,  97.20 ];  // Discount Belicheck (Harrision)
-var porScores = [ 134.05,  85.70,  94.75,  82.70,  88.80, 107.90 ];  // Porkchops (Noah)
-var tenScores = [ 142.90, 140.70,  90.55, 149.65, 106.15, 104.35 ];  // Tenacious Anus (Jacob)
+var iceScores = [ 113.75, 128.15,  92.50, 123.70, 136.35,  92.85, 100.65 ];  // Iceotopes (John)
+var jabScores = [ 142.85, 157.45, 142.10, 120.15, 181.30, 113.55, 122.45 ];  // Najee By Nature  (Matt)
+var belScores = [ 110.85, 126.30, 126.65, 126.10, 125.00,  97.20, 122.45 ];  // Discount Belicheck (Harrision)
+var porScores = [ 134.05,  85.70,  94.75,  82.70,  88.80, 107.90, 148.10 ];  // Porkchops (Noah)
+var tenScores = [ 142.90, 140.70,  90.55, 149.65, 106.15, 104.35, 105.70 ];  // Tenacious Anus (Jacob)
 
 
 // 0 - not in playoffs yet
@@ -96,16 +96,31 @@ var week5_txt = "Well, obviously the <b>Naturals</b> are the class of this leagu
 var week6_txt = "A low scoring affair this week, only two teams reaching their projected scores, and this weeks high score only matching the previous lowest weekly " +
                 "high score.  Good defense? Bad offense? Or the start of the dreaded Bye weeks? Aaaaaahhh!!!<br><br>" +
                 "I haven't actually seen it but I'm sure the <b>Chops</b> are thanking (at least mentally) the Phat " +
-                "for K Walker as they rode his 19 points to overcome the <b>Anus</b> and pick up their second win.  The Phat sure would have liked to have Walker " +
-                "still on their bench as his replacement roared to a 2.5 pt effort! Whoo! But wait, seems the <b>Phat</b> still won, as only one player for the " +
-                "<b>Topes</b> managed to meet/exceed their projections - a whole team effort there!<br><br>You know you're blessed by <b>Nature</b> when you can score " +
-                "your lowest total of the year and still beat the <b>Bellies</b> by almost 20. Both the <b>Predators</b> and the <b>Tide</b> also failed to reach their " +
-                "projected totals this week but the Preds managed to miss it by a larger amount to end up getting rolled...<br><br>And in the final matchup, the " +
-                "<b>Seamen</b> didn't help themselves playing an inactive WR but there was more off there than that as they lost to the <b>Brownies</b> by almost 40 " +
-                "pts to stay winless.  Not to fear though, in the Bizarro world for fantasy football, they're almost certainly likely to rise up next wwek to topple " +
-                "a first place team - that's how it works you know!";
+                "for K Walker as they rode his 19 points to overcome the <b>Anus</b> and pick up their second win.  The " +
+                "Phat sure would have liked to have Walker still on their bench as his replacement roared to a 2.5 pt " +
+                "effort! Whoo! But wait, seems the <b>Phat</b> still won, as only one player for the <b>Topes</b> managed " +
+                "to meet/exceed their projections - a whole team effort there!<br><br>You know you're blessed by " +
+                "<b>Nature</b> when you can score your lowest total of the year and still beat the <b>Bellies</b> by " +
+                "almost 20. Both the <b>Predators</b> and the <b>Tide</b> also failed to reach their projected totals " +
+                "this week but the Preds managed to miss it by a larger amount to end up getting rolled...<br><br>And " +
+                "in the final matchup, the <b>Seamen</b> didn't help themselves playing an inactive WR but there was " +
+                "more off there than that as they lost to the <b>Brownies</b> by almost 40 pts to stay winless.  Not to " +
+                "fear though, in the Bizarro world for fantasy football, they're almost certainly likely to rise up next " +
+                "week to topple a first place team - that's how it works you know!";
 
-var week7_txt = "Wait for it!!!!";
+var week7_txt = "In the most exciting contest of the weekend, not over till the end of MNF, the <b>Pork</b> are once again thanking " +
+                "the Phat (Yummm, pork fat!) for KW III after barely chopping down the <b>Tators</b> this week to remain undefeated " +
+                "since being gifted the gifted running back. Course thats only to reach 3-4 for the year but hey, beats the " +
+                "alternative, eh? Right back into the thick of things. <br><br>The <b>Bastards</b> on the other hand " +
+                "are going Ken Who? as they avoided any hint of an upset to the <b>Boat Boys</b>, winning by almost 40 pts... " +
+                "<br><br>So what do you do when half your roster is out on bye? Normally you score a lot less points amd lose to " +
+                "whatever lucky team happens to be playing you that week.  Well, the <b>All Naturals</b> scored less points than  " +
+                "normal as expected but forgot to do the losing part as the <b>Icicles</b> lineup froze up in the spotlight.<br><br>" +
+                "Speaking of freezing up... apparently the <b>Tide</b> doesn't roll when frozen over either, chillingly losing to " +
+                "<b>Coco Nibbs</b> by almost 50! Whew, good effort there boy, can't even blame it on Bye weeks! <br><br>And in our final " +
+                "contest(?) the <b>Bell Boys</b> tied the Najee's for lowest winning score of the week (with low winning scores " +
+                "pretty much being their modus operandi all year, not having scored over 126 pts all year but still sitting at 5-2), " +
+                "taking down the <b>Loose Butt</b>. Make sure you're standing upwind of that stinker.";
 
 var week8_txt = "Wait for it!!!!";
 
